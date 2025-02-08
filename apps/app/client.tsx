5b88a8e29f82a254883be61c278583d65571478e
Mohamad Hanifan <mohamad.hanifan@efishery.com>
2025-02-09 05:58:39 +0700

    feat: change client app

--- apps/app/client.tsx
+++
@@ -1,9 +0,0 @@
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/start'
-import { createRouter } from './router'
-
-const router = createRouter({
-    scrollRestoration: true,
-})
-
-hydrateRoot(document!, <StartClient router={router} />)
